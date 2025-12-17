bcd97c20afc598a08b6916ee9bd5d3b8b8e5a46b
bala-amypo <bala@amypo.in>
2025-12-17 09:04:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/studentEntity.java
+++ src/main/java/com/example/demo/entity/studentEntity.java
@@ -9,13 +9,25 @@ public class studentEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank()
+    @NotBlank(message="Name is not valid")
     private String name;
 
     @Email(message="Email format is not valid")
     private String email;
 
     public void setId(Long id){
-        
+        this.id=id;
+    }
+    public Long getId(){
+        return id;
+    }
+
+    public studentEntity{
+
+    }
+    public studentEntity(Long id,String name,String email){
+        this.id=id;
+        this.name=name;
+        this.email=email;
     }
 }
