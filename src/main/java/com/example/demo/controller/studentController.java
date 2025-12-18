64703ae6f64b8b322ce31b58b08f0e5416895775
bala-amypo <bala@amypo.in>
2025-12-18 09:17:31 +0000

    User commit

--- src/main/java/com/example/demo/controller/studentController.java
+++ src/main/java/com/example/demo/controller/studentController.java
@@ -1,35 +1,45 @@
 package com.example.demo.controller;
 
 import java.util.List;
+
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.entity.*;
-import com.example.demo.service.*;
+import com.example.demo.entity.studentEntity;
+import com.example.demo.service.studentService;
 
 @RestController
+@RequestMapping("/student")
 public class studentController {
 
     @Autowired
-    studentService service;
+    private studentService service;
 
     @GetMapping("/getAllStudent")
-    public List<studentEntity> getAll(){
+    public List<studentEntity> getAll() {
         return service.getAll();
     }
 
     @PostMapping("/add")
-    public studentEntity addStudent(@Valid  @RequestBody studentEntity student ){
+    public studentEntity addStudent(@Valid @RequestBody studentEntity student) {
         return service.addStudent(student);
     }
 
-    @GetMapping("/get/{Id}")
-    public studentEntity getbyId(@PathVariable Long id){
+    @GetMapping("/get/{id}")
+    public studentEntity getbyId(@PathVariable Long id) {
         return service.getbyId(id);
     }
-    
+
     @PutMapping("/update/{id}")
-    public studentEntity updatebyId(@PathVariable Long id @Valid @RequestBody 
+    public studentEntity updateBy(@PathVariable Long id,@Valid @RequestBody studentEntity newstu) {
+        return service.updateBy(id, newstu);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteByID(@PathVariable Long id) {
+        service.deleteByID(id);
+        return "Deleted Successfully!";
+    }
 }
